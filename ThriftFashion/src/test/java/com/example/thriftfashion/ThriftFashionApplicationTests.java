92009bcc0d599f4ef54ffca4c586e5f5d7529595
Rinowo <tunganhhh205@gmail.com>
2022-08-10 09:36:17 +0700

    1

---
+++ ThriftFashion/src/test/java/com/example/thriftfashion/ThriftFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thriftfashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThriftFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
